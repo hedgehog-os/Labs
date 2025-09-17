8453e4f49bd819265b0aa41dbf9cc15712de01a4
Egor <explayeroha@gmail.com>
2025-09-17 13:37:39 +0300

    Implemented a working interface for multisets

--- SDIS/lab1/Multiset_Interface.py
+++ SDIS/lab1/Multiset_Interface.py
@@ -1,14 +1,17 @@
 from Multiset import Multiset
 
 def main():
-    while True:
-        print('''
+
+    multisets = {}
+
+    print('''
                 Enter multiset in the following format: A = {a, b, {c, d}, a}
                 Available commands:
                     list            check list of multisets
-                    delete a        remove element from multiset
-                    ndelete a:2     remove a specified number of instances of an element
-                    is empty A      check for empty multiset
+                    show A          show multiset A
+                    delete A a      remove element from multiset
+                    ndelete A a:2   remove a specified number of instances of an element
+                    isempty A       check for empty multiset
                     cardinality A   count cardinality A
                     rm A            remove multiset A
                     A + B           union A and B
@@ -17,8 +20,92 @@ def main():
                     A -= B          save result substaction in A
                     A * B           intersection between A and B
                     A *= B          save result intersection in A
-                    bolen A         create bolean from A
+                    bolean A        create bolean from A
+                    help            show all available commands
                     exit            stop programm
                 ''')
     
+    while True:
+    
         choice = input('')
+
+        parts = choice.split()
+
+        if parts[0] == 'list':
+            for name in multisets:
+                print(f"{name} = {multisets[name]}")
+        
+        elif parts[0] == 'show' and parts[1] in multisets:
+            print(f"{parts[1]} = {multisets[parts[1]]}")
+
+        elif '=' in choice:
+            name, mul = choice.split('=', 1)
+            multisets[name.strip()] = Multiset(mul.strip())
+
+        elif parts[0] == 'delete' and parts[1] in multisets and parts[2] in multisets[parts[1]].multiset:
+            multisets[parts[1]].delete(parts[2])
+
+        elif parts[0] == 'ndelete' and parts[1] in multisets and ':' in parts[2]:
+            elem, num = parts[2].split(':', 1)
+            multisets[parts[1]].ndelete(elem, int(num))
+        
+        elif parts[0] == 'isempty' and parts[1] in multisets:
+            print(f"{parts[1]} is empty: {multisets[parts[1]].is_empty()}")
+
+        elif parts[0] == 'cardinality':
+            print(f"Cardinality of {parts[1]}: {multisets[parts[1]].cardinality()}")
+
+        elif parts[0] == 'rm':
+            del multisets[parts[1]]
+
+        elif parts[1] == '+' and parts[0] in multisets and parts[2] in multisets:
+            print(multisets[parts[0]] + multisets[parts[2]])
+
+        elif parts[1] == '+=' and parts[0] in multisets and parts[2] in multisets:
+            multisets[parts[0]] += multisets[parts[2]]
+
+        elif parts[1] == '-' and parts[0] in multisets and parts[2] in multisets:
+            print(multisets[parts[0]] - multisets[parts[2]])
+
+        elif parts[1] == '-=' and parts[0] in multisets and parts[2] in multisets:
+            multisets[parts[0]] -= multisets[parts[2]]
+
+        elif parts[1] == '*' and parts[0] in multisets and parts[2] in multisets:
+            print(multisets[parts[0]] * multisets[parts[2]])
+
+        elif parts[1] == '*=' and parts[0] in multisets and parts[2] in multisets:
+            multisets[parts[0]] *= multisets[parts[2]]
+
+        elif parts[0] == 'bolean' and parts[1] in multisets:
+            result = multisets[parts[1]].bolean()
+            print(f"Булеан множества {parts[1]} содержит {len(result)} подмножеств:")
+            for subset in result:
+                print(subset)
+
+        elif parts[0] == 'help':
+            print('''
+                Enter multiset in the following format: A = {a, b, {c, d}, a}
+                Available commands:
+                    list            check list of multisets
+                    show A          show multiset A
+                    delete A a      remove element from multiset
+                    ndelete A a:2   remove a specified number of instances of an element
+                    isempty A       check for empty multiset
+                    cardinality A   count cardinality A
+                    rm A            remove multiset A
+                    A + B           union A and B
+                    A += B          save result union in A
+                    A - B           substract from A to B
+                    A -= B          save result substaction in A
+                    A * B           intersection between A and B
+                    A *= B          save result intersection in A
+                    bolean A        create bolean from A
+                    help            show all available commands
+                    exit            stop programm
+                ''')
+
+        elif 'exit' in parts[0]:
+            return
+        
+        else:
+            print('Wrong command. Try again.')
