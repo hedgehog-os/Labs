b0ac9a757805c31e3f2e2d4125bcfa666ee0ebc1
Egor <explayeroha@gmail.com>
2025-10-17 20:37:28 +0300

    Implement class Protocol

---
+++ SDIS/lab2/Protocol.py
@@ -0,0 +1,14 @@
+class Protocol:
+
+    def __init__(self, procedure_steps: list[str] = None,
+                 safety_protocol: str = None,
+                 approval_date: str = None,
+                 expiration_date: str = None,
+                 linked_experiment_ids: list[int] = None
+                 ):
+        
+        self.procedure_steps = procedure_steps
+        self.safety_protocol = safety_protocol
+        self.approval_date = approval_date
+        self.expiration_date = expiration_date
+        self.linked_experiment_ids = linked_experiment_ids
