db2556b745e274410293a2d07ddc17dfeecc2199
Egor <explayeroha@gmail.com>
2025-10-17 20:17:00 +0300

    add classes Document and Report with their atributes

---
+++ SDIS/lab2/Document.py
@@ -0,0 +1,82 @@
+class Document:
+    
+    statuses = {
+        'draft', 'submitted', 'under_review', 'approved',
+        'rejected', 'archived', 'final', 'expired', 'deleted'
+    }
+
+    confidentiality_levels = {
+        'public', 'internal', 'restricted', 
+        'confidential', 'secret', 'top_secret'
+    }
+
+    formats = {
+        'pdf', 'docx', 'txt', 'md','html',
+        'xlsx', 'csv', 'json', 'xml',
+        'latex', 'py', 'cpp'
+    }
+
+    def __init__(path: str,
+                 self, id: int,
+                 author_id: int,
+                 page_count: int,
+                 title: str = None,
+                 desc: str = None,
+                 created_at: str = None, 
+                 updated_at: str = None,
+                 tags :str = None,
+                 keywords: str = None,
+                 language: str = None,
+                 format: str = None,
+                 word_count: int = None,
+                 file_size: int = None,
+                 confidentiality_level: str = 'public',
+                 status: str = 'draft',
+                 ):
+        
+        self.document_id = id
+        self.title = title
+        self.description = desc
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.author_id = author_id
+        self.status = status
+        self.tags = tags or []
+        self.keywords = keywords or []
+        self.confidentiality_level = confidentiality_level
+        self.language = language
+        self.format = format
+        self.page_count = page_count
+        self.word_count = word_count
+        self.file_size = file_size
+        self.path = path
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        if value not in self.statuses:
+            raise ValueError(f'Недопустимый статус: {value}')
+        self._status = value
+
+    @property
+    def confidentiality_level(self):
+        return self._confidential_level
+
+    @confidentiality_level.setter
+    def confidentility_level(self, value):
+        if value not in self.confidentiality_levels:
+            raise ValueError(f'Недопустимый уровень конфиденциальности: {value}')
+        self._confidentality_level = value
+
+    @property
+    def format(self):
+        return self._format
+
+    @format.setter
+    def format(self, value):
+        if value not in self.formats:
+            raise ValueError(f'Недопустимый уровень конфиденциальности: {value}')
+        self.format = value
---
+++ SDIS/lab2/Report.py
@@ -0,0 +1,39 @@
+class Report:
+    
+    chart_types = {
+        'bar', 'line', 'pie', 'scatter',
+        'histogram', 'area', 'bubble',
+        'heatmap', 'radar', 'boxplot'
+    }
+
+    def __init__(self, summary: str = None,
+                 result_summary: str = None,
+                 reviewer_comments: list[str] = None,
+                 reviewer_rating: str = None,
+                 reviewer_text: str = None,
+                 review_date: str = None,
+                 chart_type: str = None,
+                 chart_date: dict = None,
+                 insight_summary: str = None
+                 ):
+        
+        self.summary = summary
+        self.result_summary = result_summary
+        self.reviewer_comments = reviewer_comments
+        self.reviewer_comments = reviewer_rating
+        self.reviewer_text = reviewer_text
+        self.review_date = review_date
+        self.chart_type = chart_type
+        self.chart_date = chart_date
+        self.insight_summary = insight_summary
+
+
+    @property
+    def chart_type(self):
+        return self._chart_type
+
+    @chart_type.setter
+    def chart_type(self, value):
+        if value not in self.chart_types:
+            raise ValueError(f'Недопустимый статус: {value}')
+        self._chart_type = value
