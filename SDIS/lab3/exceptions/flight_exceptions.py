a6c8e07f4fc24ded099c15e07ce80f7fe4441ab7
Egor <explayeroha@gmail.com>
2025-11-06 22:17:51 +0300

    Add exceptions

--- SDIS/lab3/exceptions/flight_exceptions.py
+++ SDIS/lab3/exceptions/flight_exceptions.py
@@ -5,3 +5,7 @@ class FlightOverbookedException(Exception):
 class GateConflictException(Exception):
     def __init__(self, gate_number: str) -> None:
         super().__init__(f"Gate {gate_number} is already assigned to another flight.")
+
+class FlightCapacityExceededException(Exception):
+    def __init__(self, flight_number: str) -> None:
+        super().__init__(f"Flight {flight_number} is at full capacity")
