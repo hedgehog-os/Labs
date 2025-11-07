5b94c29f61c921723afcd5ce7ae6193fceeeff90
Egor <explayeroha@gmail.com>
2025-11-07 21:24:31 +0300

    Add README.md

--- SDIS/lab3/models/operations/security_check.py
+++ SDIS/lab3/models/operations/security_check.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from models.passenger.passenger import Passenger
