bfcf7b76a55fbefb28365f2efb026d9073d6a56a
Egor <explayeroha@gmail.com>
2026-01-23 18:02:06 +0300

    Add all labs

--- OS/lab1.sh
+++ OS/lab1.sh
@@ -2,21 +2,19 @@
 
 # Проверка аргументов
 if [ $# -ne 2 ]; then
-    echo "Использование: $0 <строка> <каталог>"
-    exit 1
+  echo "Использование: $0 <строка> <каталог>"
+  exit 1
 fi
 
 SEARCH_STRING="$1"
 SEARCH_DIR="$2"
 
 if [ ! -d "$SEARCH_DIR" ]; then
-    echo "Ошибка: $SEARCH_DIR не является каталогом"
-    exit 1
+  echo "Ошибка: $SEARCH_DIR не является каталогом"
+  exit 1
 fi
 
 grep -rl "$SEARCH_STRING" "$SEARCH_DIR" 2>/dev/null | while read -r file; do
-    size=$(stat -c%s "$file")
-    echo "Файл: $file | Размер: $size байт"
+  size=$(stat -c%s "$file")
+  echo "Файл: $file | Размер: $size байт"
 done
-
-
